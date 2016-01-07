b54ad5bd797877ba2d025e6fe4986b8f5a72df6c
David Wang <lijen_dwang@hotmail.com>
2016-01-07 14:47:51 -0800

    commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $name = 'Jim';
+            $what = 'geek. ';
+            $level = 10;
+            echo 'Hi, my name is '.$name,'. and I am a level '.$level.' 
+            '.$what;
+
+            $hoursworked = 10;
+            $rate = 12;
+            $total = $hoursworked * $rate;
+            echo 'You owe me '.$total;
+        ?>
+    </body>
+</html>
